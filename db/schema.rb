053ed3787887411aa75a3ebcbbf468dc8585127d
Jason Waldrip <jason@waldrip.net>
2016-02-21 11:34:11 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220230554) do
+ActiveRecord::Schema.define(version: 20160221183226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160220230554) do
     t.string   "slack_id",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   add_index "channels", ["slack_id"], name: "index_channels_on_slack_id", unique: true, using: :btree

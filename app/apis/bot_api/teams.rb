64ee9ac414ba1d86e9a6aa95981a8b9025614af9
Jason Waldrip <jason@waldrip.net>
2016-02-20 23:31:37 -0700

    add logo url

--- app/apis/bot_api/teams.rb
+++ app/apis/bot_api/teams.rb
@@ -11,6 +11,7 @@ BotApi.define_resource :teams do
   attribute :name, types.String, 'The name of the Slack Team.'
   attribute :slack_bot_id, types.String, 'The id of the Slack bot.'
   attribute :slack_bot_token, types.String, 'The token of the Slack bot.'
+  attribute :logo_url, types.String, 'The URL to the logo.'
   attribute :active, types.Boolean, 'Is the team active.'
 
   relates_to_many :users do

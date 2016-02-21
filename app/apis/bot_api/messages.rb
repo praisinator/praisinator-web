c0f1d57ab81ced67bf2b162ae37c9cb8ef8b128d
Jason Waldrip <jason@waldrip.net>
2016-02-21 10:57:07 -0700

    fix api

--- app/apis/bot_api/messages.rb
+++ app/apis/bot_api/messages.rb
@@ -7,9 +7,8 @@ BotApi.define_resource :messages do
     scope.new slack_id: context.request_id, **context.request_attributes
   end
 
-  attribute :name, types.String, 'The name of the Slack Team.'
   attribute :user_slack_id, types.String, 'The user id who sent the message.'
-  attribute :body, types.String, 'The message of the body.'
+  attribute :content, types.String, 'The message of the body.'
   attribute :timestamp, types.Float, 'The timestamp of the message.'
 
   relates_to_one :channel, resource: :channels

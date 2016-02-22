30e58482945dd4978a6ac4949a9e726479584cf4
Jason Waldrip <jason@waldrip.net>
2016-02-21 21:39:33 -0700

    normalize tones

--- app/models/tone.rb
+++ app/models/tone.rb
@@ -1,6 +1,8 @@
 class Tone < ActiveRecord::Base
   belongs_to :message
 
+  before_create :normalize
+
   def self.composite
     t = arel_table
     select(
@@ -20,6 +22,42 @@ class Tone < ActiveRecord::Base
       t[:social_agreeableness].average.as('social_agreeableness'),
       t[:social_neuroticism].average.as('social_neuroticism')
     ).to_a.first
+  end
+
+  def normalize
+    select_greatest_emotion
+    select_greatest_intent
+    select_greatest_social
+    self
+  end
+
+  private
+
+  def select_greatest_intent
+    cols = %w{writing_analytical writing_confident writing_tentative}
+    attrs = attributes.slice(*cols)
+    return if attrs.values.all?(&:zero?)
+    *lesser, greatest = attrs.sort_by(&:last).to_h.keys
+    lesser.each { |attr| write_attribute attr, 0.0 }
+    write_attribute(greatest, 1.0)
+  end
+
+  def select_greatest_emotion
+    cols = %w{emotional_anger emotional_disgust emotional_fear emotional_joy emotional_sadness}
+    attrs = attributes.slice(*cols)
+    return if attrs.values.all?(&:zero?)
+    *lesser, greatest = attrs.sort_by(&:last).to_h.keys
+    lesser.each { |attr| write_attribute attr, 0.0 }
+    write_attribute(greatest, 1.0)
+  end
+
+  def select_greatest_social
+    cols = %w{social_openness social_conscientiousness social_extraversion social_agreeableness}
+    attrs = attributes.slice(*cols)
+    return if attrs.values.all?(&:zero?)
+    *lesser, greatest = attrs.sort_by(&:last).to_h.keys
+    lesser.each { |attr| write_attribute attr, 0.0 }
+    write_attribute(greatest, 1.0)
   end
 
 end

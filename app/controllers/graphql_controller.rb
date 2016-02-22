c01c791278e13e597d23481c6f227f57802f42fa
Jason Waldrip <jason@waldrip.net>
2016-02-21 20:32:24 -0700

    fix

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,7 +3,7 @@ class GraphqlController < ApplicationController
   def query
     def execute
       puts '-' * 100, GraphQLFormatter.new(params[:query]).to_s, '-' * 100
-      result = RelaySchema.execute(params[:query], debug: true, variables: params[:variables])
+      result = Schema.execute(params[:query], debug: true, variables: params[:variables])
       render json: result
     end
   end

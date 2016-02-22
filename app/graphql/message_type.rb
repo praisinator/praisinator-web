155503663473c42bbcd2e52e5c8a40dd5e84ad71
Jason Waldrip <jason@waldrip.net>
2016-02-21 21:39:52 -0700

    secure messages

--- app/graphql/message_type.rb
+++ app/graphql/message_type.rb
@@ -4,8 +4,6 @@ MessageType = GraphQL::ObjectType.define do
   interfaces [NodeIdentification.interface]
   global_id_field :id
 
-  field :content, !types.String
-
   field :tone, -> { ToneType }
   field :team, -> { TeamType }
   field :channel, -> { ChannelType }

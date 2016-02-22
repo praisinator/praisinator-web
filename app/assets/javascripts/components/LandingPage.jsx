b3773abaa7ccb2a3fd1a6cabb3c2c5150b90e110
Logan Sears <logan.sears@brandfolder.com>
2016-02-21 18:55:19 -0700

    Fix scoping on slack button

--- app/assets/javascripts/components/LandingPage.jsx
+++ app/assets/javascripts/components/LandingPage.jsx
@@ -25,7 +25,7 @@ class LandingPage extends React.Component {
       </Row>
       <Row>
       <Col md={4} sm={4}>
-        <div> <a href="https://slack.com/oauth/authorize?scope=bot,team%3Aread,users%3Aread,channels%3Aread&amp;client_id=9094370912.22344990885">
+        <div> <a href="https://slack.com/oauth/authorize?scope=bot,commands,team%3Aread,users%3Aread,channels%3Aread,channels%3Ahistory&client_id=9094370912.22344990885">
   <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x "/>
 </a></div>
       </Col>

e008ca02379774507b35c02013f50d3a014e1ace
Logan Sears <logan.sears@brandfolder.com>
2016-02-22 00:01:01 -0700

    Fix spelling errors

--- app/assets/javascripts/components/LandingPage.jsx
+++ app/assets/javascripts/components/LandingPage.jsx
@@ -17,11 +17,11 @@ class LandingPage extends React.Component {
       <Row>
         <Col lg={12} md={12} sm={12}>
           <h1> Praisinator</h1>
-          <p> What's the mood of your team today? Well, now you don't have to
+          <p> What's the mood of your team today? Not sure? Well, now you don't have to
           fret, worry, or pour over thousands of messages searching for a clue.
-          With the help of state-of-the-art technology you, <strong>yes you</strong>, can easy identify the
+          With the help of state-of-the-art technology you, <strong>yes you</strong>, can easily identify the
           mood of your Slack teams and channels. No more sleepless nights wondering what your
-          team is feeling. Are they happy, sad, angry, fearful...? With Praisinator, you now
+          team is feeling. Are they happy, sad, angry, fearful...? With Praisinator, you
           have the answer.
            </p>
            <ButtonToolbar>
@@ -38,7 +38,7 @@ class LandingPage extends React.Component {
           <h3>It starts with a message </h3>
           <p> With the help of apps like Slack, teams are communicating more than ever. Sharing ideas, frustrations,
           problem-solving, posting gifs; every time your team starts a conversation, our app starts crunching.
-          So, we're there through all the conversation's ups and downs. </p>
+          We're there through all the conversation's ups and downs. </p>
       </Col>
       <Col md={4} sm={4}>
           <div className="slack-logo">
@@ -47,14 +47,14 @@ class LandingPage extends React.Component {
           <h3>It captures a mood </h3>
           <p> There's a lot of chatter and emotions that occur throughout the day.
               When you're just popping in and out of the conversation, how can you really tell if you're reading the mood properly?
-              Maybe you just caught a bad moment. Now you can get a sense of the overall mood of each team and channel without the wonder or worry. </p>
+              Now you can get a sense of your team's mood without all the wonder or worry. </p>
       </Col>
       <Col md={4} sm={4}>
         <div className="slack-logo">
           <i className="fa fa-pie-chart"></i>
         </div>
         <h3> It tells a story  </h3>
-        <p> You can delve into the day, week or hour of your teams experiences and see the emotions, sentiment,
+        <p> You can delve into the day, week, or hour of your teams experiences and see the emotions, sentiment,
             and attitudes. Become an analytical master of mood, and uncover the feelings and state of mind
             that tells the story of a teams life together. </p>
       </Col>

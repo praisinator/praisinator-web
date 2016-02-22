21635b7b8cd7589e29de2b4944de1b9e26ea5429
Jason Waldrip <jason@waldrip.net>
2016-02-21 20:34:13 -0700

    fix method

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,11 +1,9 @@
 class GraphqlController < ApplicationController
 
   def query
-    def execute
-      puts '-' * 100, GraphQLFormatter.new(params[:query]).to_s, '-' * 100
-      result = Schema.execute(params[:query], debug: true, variables: params[:variables])
-      render json: result
-    end
+    puts '-' * 100, GraphQLFormatter.new(params[:query]).to_s, '-' * 100
+    result = Schema.execute(params[:query], debug: true, variables: params[:variables])
+    render json: result
   end
 
 end
